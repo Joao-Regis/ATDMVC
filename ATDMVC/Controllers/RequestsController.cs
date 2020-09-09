81a2b382c939fb41bf4d730a047d56b8d228393b
João <40552750+Joao-Regis@users.noreply.github.com>
2020-09-08 20:26:01 -0700

    Modify RequestsController: Add applicationDbContext _db ctor _db = db Add API Calls for HTTPGet and HTTPDelete from Razor Version modifiy Request to Requests. Modify RequestId to RequestID

--- ATDMVC/Controllers/RequestsController.cs
+++ ATDMVC/Controllers/RequestsController.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ATDMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ATDMVC.Controllers
 {
     public class RequestsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public RequestsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
+
+        #region API Calls
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Requests.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var requestFromDb = await _db.Requests.FirstOrDefaultAsync(u => u.RequestID == id);
+            if (requestFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+
+            _db.Requests.Remove(requestFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete succesful" });
+        }
+
+        #endregion
+
+
+
     }
 }
